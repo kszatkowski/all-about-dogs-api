89137f9d323d3f67678083dcd49514b588e62b25
kszatkowski <kszatkowski@outlook.com>
2024-05-07 15:19:37 +0200

    Cors
    Added cors to application

--- src/utils/app.ts
+++ src/utils/app.ts
@@ -2,11 +2,13 @@ import 'dotenv/config';
 import express from 'express';
 import { dogBreedsRoutes, imagesRoutes } from '@components';
 import { errorMiddleware } from '@middlewares';
+import cors from 'cors';
 
 export function createApp() {
   const app = express();
   app.use(express.json());
 
+  app.use(cors());
   app.use(dogBreedsRoutes);
   app.use(imagesRoutes);
 
